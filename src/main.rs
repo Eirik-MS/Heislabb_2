f67c063a946948921198b6e8fe925ec4fd27f74b
SuperFighterX <77638149+SuperFighterX@users.noreply.github.com>
2025-01-22 11:22:04 +0100

    lets gooooo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread::*;
+
+use network_rust::udpnet;
+
+mod elevator;
+use crate::elevator as elev;
+
+fn main() -> std::io::Result<()> {
+    thread::spawn(||elevator_start())
 }
