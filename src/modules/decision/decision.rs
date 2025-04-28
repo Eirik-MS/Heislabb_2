f3f042dd23ee93f0e74ad962fb23e6db83fd427e
Eirik-MS <eirikmathias@gmail.com>
2025-04-28 02:09:14 +0200

    added back into noorder

--- src/modules/decision/decision.rs
+++ src/modules/decision/decision.rs
@@ -380,7 +380,9 @@ impl Decision {
                                                 else {
                                                     local_order.barrier.clear(); 
                                                     local_order.source_id.clear();
-                                                    
+                                                    if *lid == self.local_id {
+                                                        let _ = self.order_completed_other_tx.send(local_order.clone()).await;
+                                                    }
                                                    
                                                 }
                                             }
